74b43cda31ee7b636a8cb737e1e4b9466a850088
kingov.alexey <kingov_alexey@mail.ru>
2021-05-19 23:25:20 +0600

    11

--- way-of-samuray/src/App.js
+++ way-of-samuray/src/App.js
@@ -1,15 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
-import Header from './Header';
-import Technologies from './Technologies';
-import Footer from './Footer';
 
 function App() {
   return (
-    <div>
-      <Header />
-      <Technologies />
-      <Footer />
+    <div className="app-wrapper">
+      <header className="header">
+        <img src="http://www.vkrg.kz/images/picture-136.png" />
+      </header>
+      <nav className="nav">
+        <div>
+          <a href="#Profile">Profile</a>
+        </div>
+        <div>
+          <a href="#Messages">Messages</a>
+        </div>
+        <div>
+          <a href="#News">News</a>
+        </div>
+        <div>
+          <a href="#Music">Music</a>
+        </div>
+        <div>
+          <a href="#Settings">Settings</a>
+        </div>
+      </nav>
+      <div className="content">
+        <div>
+          <img src="http://www.vkrg.kz/images/img1.jpg" />
+        </div>
+        <div>ava + description</div>
+        <div>
+          my posts
+          <div>New post</div>
+          <div>post 1</div>
+          <div>post 2</div>
+          <div>post 3</div>
+        </div>
+      </div>
     </div>
   );
 }
