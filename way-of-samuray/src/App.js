65d5d80f89e382a380dc593635ecd63303525b1e
kingov.alexey <kingov_alexey@mail.ru>
2021-05-19 23:40:42 +0600

    12

--- way-of-samuray/src/App.js
+++ way-of-samuray/src/App.js
@@ -1,42 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './componets/Header';
+import Navbar from './componets/Navbar';
+import Profile from './componets/Profile';
 
 function App() {
   return (
     <div className="app-wrapper">
-      <header className="header">
-        <img src="http://www.vkrg.kz/images/picture-136.png" />
-      </header>
-      <nav className="nav">
-        <div>
-          <a href="#Profile">Profile</a>
-        </div>
-        <div>
-          <a href="#Messages">Messages</a>
-        </div>
-        <div>
-          <a href="#News">News</a>
-        </div>
-        <div>
-          <a href="#Music">Music</a>
-        </div>
-        <div>
-          <a href="#Settings">Settings</a>
-        </div>
-      </nav>
-      <div className="content">
-        <div>
-          <img src="http://www.vkrg.kz/images/img1.jpg" />
-        </div>
-        <div>ava + description</div>
-        <div>
-          my posts
-          <div>New post</div>
-          <div>post 1</div>
-          <div>post 2</div>
-          <div>post 3</div>
-        </div>
-      </div>
+      <Header />
+      <Navbar />
+      <Profile />
     </div>
   );
 }
---
+++ way-of-samuray/src/componets/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img src="http://www.vkrg.kz/images/picture-136.png" />
+    </header>
+  );
+};
+
+export default Header;
