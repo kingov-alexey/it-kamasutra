5469d54e4ea9f295d8a2f1cea83e5157852b56d3
kingov.alexey <kingov_alexey@mail.ru>
2021-05-24 22:59:23 +0600

    21

--- way-of-samuray/src/componets/Dialogs/Dialogs.jsx
+++ way-of-samuray/src/componets/Dialogs/Dialogs.jsx
@@ -3,9 +3,21 @@ import s from './Dialogs.module.css';
 
 const Dialogs = props => {
   return (
-    <div>
-      Dialogs
-      <div>Диалоги</div>
+    <div className={s.dialogs}>
+      <div className={s.dialogsItems}>
+        <div className={s.dialog + ' ' + s.active}>Игорь1</div>
+        <div className={s.dialog}>Игорь2</div>
+        <div className={s.dialog}>Аскар</div>
+        <div className={s.dialog}>Толян</div>
+        <div className={s.dialog}>Витек</div>
+        <div className={s.dialog}>Стас</div>
+        <div className={s.dialog}>Азиз</div>
+      </div>
+      <div className={s.messages}>
+        <div className={s.message}>Hi</div>
+        <div className={s.message}>asdfasdf</div>
+        <div className={s.message}>asdfasdf</div>
+      </div>
     </div>
   );
 };
