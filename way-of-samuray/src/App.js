ee33a9d2ebabed4f8406403c7702e7a25f00eb08
kingov.alexey <kingov_alexey@mail.ru>
2021-05-27 15:30:26 +0600

    30

--- way-of-samuray/src/App.js
+++ way-of-samuray/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
 import { Route } from 'react-router';
-import { BrowserRouter } from 'react-router-dom';
 import Header from './componets/Header/Header';
 import Navbar from './componets/Navbar/Navbar';
 import Profile from './componets/Profile/Profile';
@@ -12,26 +11,18 @@ import Settings from './componets/Settings/Settings';
 
 function App(props) {
   return (
-    <BrowserRouter>
-      <div className="app-wrapper">
-        <Header />
-        <Navbar />
+    <div className="app-wrapper">
+      <Header />
+      <Navbar />
 
-        <div className="app-wrapper-content">
-          {/* <Route path="/profile" component={Profile} />
-          <Route path="/dialogs" component={Dialogs} />
-          <Route path="/news" component={News} />
-          <Route path="/music" component={Music} />
-          <Route path="/settings" component={Settings} /> */}
-
-          <Route path="/profile" render={() => <Profile state={props.state.profilePage} />} />
-          <Route path="/dialogs" render={() => <Dialogs state={props.state.dialogsPage} />} />
-          <Route path="/news" render={() => <News />} />
-          <Route path="/music" render={() => <Music />} />
-          <Route path="/settings" render={() => <Settings />} />
-        </div>
+      <div className="app-wrapper-content">
+        <Route path="/profile" render={() => <Profile state={props.state.profilePage} />} />
+        <Route path="/dialogs" render={() => <Dialogs state={props.state.dialogsPage} />} />
+        <Route path="/news" render={() => <News />} />
+        <Route path="/music" render={() => <Music />} />
+        <Route path="/settings" render={() => <Settings />} />
       </div>
-    </BrowserRouter>
+    </div>
   );
 }
 
