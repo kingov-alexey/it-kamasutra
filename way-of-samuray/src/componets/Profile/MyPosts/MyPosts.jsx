1b126fe5b1781e14286b7acb7624524f5846f9a3
kingov.alexey <kingov_alexey@mail.ru>
2021-06-01 23:28:29 +0600

    41

--- way-of-samuray/src/componets/Profile/MyPosts/MyPosts.jsx
+++ way-of-samuray/src/componets/Profile/MyPosts/MyPosts.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import s from './MyPosts.module.css';
 import Post from './Post/Post';
-import { updateNewPostTextActionCreator, addPostActionCreator } from './../../../redux/state';
+import {
+  updateNewPostTextActionCreator,
+  addPostActionCreator,
+} from './../../../redux/profile-reducer';
 
 const MyPosts = props => {
   let postsElements = props.posts.map(p => {
