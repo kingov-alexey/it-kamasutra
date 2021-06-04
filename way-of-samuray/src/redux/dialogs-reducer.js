06634a48d7e4185066a919b671f4c544aa55ced0
kingov.alexey <kingov_alexey@mail.ru>
2021-06-04 22:34:21 +0600

    42

--- way-of-samuray/src/redux/dialogs-reducer.js
+++ way-of-samuray/src/redux/dialogs-reducer.js
@@ -1,7 +1,28 @@
 const UPDATE_NEW_MESSAGE_BODY = 'UPDATE-NEW-MESSAGE-BODY';
 const SEND_MESSAGE = 'SEND-MESSAGE';
 
-const dialogsReducer = (state, action) => {
+let initialState = {
+  dialogs: [
+    { id: 0, name: 'Dimych' },
+    { id: 1, name: 'Alexey' },
+    { id: 2, name: 'Gary' },
+    { id: 3, name: 'Tolik' },
+    { id: 4, name: 'Vitek' },
+    { id: 5, name: 'Igor' },
+  ],
+  messages: [
+    { id: 0, message: 'Hi!' },
+    { id: 1, message: 'Help me please.' },
+    { id: 2, message: 'How I can go to CityMall?' },
+    { id: 3, message: 'Thanks!' },
+    { id: 4, message: 'asdf,' },
+    { id: 5, Message: 'asdfasdf.' },
+  ],
+
+  newMessageBody: '',
+};
+
+const dialogsReducer = (state = initialState, action) => {
   switch (action.type) {
     case UPDATE_NEW_MESSAGE_BODY:
       state.newMessageBody = action.body;
--- way-of-samuray/src/redux/profile-reducer.js
+++ way-of-samuray/src/redux/profile-reducer.js
@@ -1,7 +1,18 @@
 const ADD_POST = 'ADD-POST';
 const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
 
-const profileReducer = (state, action) => {
+let initialState = {
+  posts: [
+    { id: 0, message: 'Hi! My post1', likeCount: 27 },
+    { id: 1, message: 'Help me please. My post', likeCount: 88 },
+    { id: 2, message: 'How I can go to CityMall? My post', likeCount: 60 },
+    { id: 3, message: 'Thanks! My post', likeCount: 44 },
+  ],
+
+  newPostText: 'It-kamasutra.com',
+};
+
+const profileReducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_POST:
       let newPost = {
@@ -16,6 +27,9 @@ const profileReducer = (state, action) => {
 
     case UPDATE_NEW_POST_TEXT:
       state.newPostText = action.newText;
+      return state;
+
+    default:
       return state;
   }
 };
