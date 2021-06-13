92469ef465cf234d764f2c792258f1f5f3371112
kingov.alexey <kingov_alexey@mail.ru>
2021-06-14 00:09:57 +0600

    47

--- way-of-samuray/src/redux/profile-reducer.js
+++ way-of-samuray/src/redux/profile-reducer.js
@@ -14,20 +14,26 @@ let initialState = {
 
 const profileReducer = (state = initialState, action) => {
   switch (action.type) {
-    case ADD_POST:
+    case ADD_POST: {
       let newPost = {
         id: 5,
         message: state.newPostText,
         likeCount: 0,
       };
 
-      state.posts.push(newPost);
-      state.newPostText = '';
-      return state;
+      let stateCopy = { ...state };
+      stateCopy.posts = [...state.posts];
+      stateCopy.posts.push(newPost);
+      stateCopy.newPostText = '';
+      return stateCopy;
+    }
 
-    case UPDATE_NEW_POST_TEXT:
-      state.newPostText = action.newText;
-      return state;
+    case UPDATE_NEW_POST_TEXT: {
+      let stateCopy = { ...state };
+      stateCopy.posts = [...state.posts];
+      stateCopy.newPostText = action.newText;
+      return stateCopy;
+    }
 
     default:
       return state;
@@ -35,10 +41,9 @@ const profileReducer = (state = initialState, action) => {
 };
 
 export const addPostActionCreator = () => ({ type: ADD_POST });
-export const updateNewPostTextActionCreator = text => {
-  return {
-    type: UPDATE_NEW_POST_TEXT,
-    newText: text,
-  };
-};
+export const updateNewPostTextActionCreator = text => ({
+  type: UPDATE_NEW_POST_TEXT,
+  newText: text,
+});
+
 export default profileReducer;
