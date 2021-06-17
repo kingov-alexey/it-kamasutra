0d0599b3194e754258ab3934d3fd8f4987d6fe78
Алексей Викторович Королёв <korolev@toowto.kz>
2021-06-17 18:00:30 +0600

    49 no finish. stoped 46:00

--- way-of-samuray/src/componets/Users/Users.jsx
+++ way-of-samuray/src/componets/Users/Users.jsx
@@ -1,9 +1,33 @@
 import React from "react";
 
-const Users = ()=>{
+const Users = (props)=>{
     return (
         <div>
-            Users will be here
+           {
+
+props.users.map(u => <div>
+<span>
+    <div><img src="" alt="" /></div>
+    <div>
+        <button> follow</button>
+    </div>
+</span>
+<span>
+<span>
+    <div>{u.fullName}</div> <div>{u.status}</div>
+</span>
+<span>
+    <div>{u.location.city}</div> <div>{u.location.country}</div>
+</span>
+
+</span>
+
+</div>)
+   
+   
+   
+   
+   }
 
         </div>
     )
